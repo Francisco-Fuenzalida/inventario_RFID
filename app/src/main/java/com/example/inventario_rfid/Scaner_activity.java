b6319204b53885fb86c524645a85a1d6ea0ee7eb
Josue Ramos <josu.ramos@duocuc.cl>
2022-06-08 21:24:59 -0400

    Visual en Scaner
    Se elimina RFIDHandler y se agregan cambios visuales en Scaner

--- app/src/main/java/com/example/inventario_rfid/Scaner_activity.java
+++ app/src/main/java/com/example/inventario_rfid/Scaner_activity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.widget.Toolbar;
-import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
@@ -15,71 +14,21 @@ import android.widget.Toast;
 
 import com.zebra.rfid.api3.TagData;
 
-public class Scaner_activity extends AppCompatActivity implements RFIDHandler.ResponseHandlerInterface{
-    public TextView statusTextViewRFID = null;
-    private TextView textrfid;
-    private TextView testStatus;
-    RFIDHandler rfidHandler;
+public class Scaner_activity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scaner);
 
-        rfidHandler = new RFIDHandler();
-        rfidHandler.onCreate(this);
+
         Button btn_volver = (Button) findViewById(R.id.btn_volver_scan);
-        btn_volver.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
+        btn_volver.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 finish();
             }
         });
     }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        rfidHandler.onPause();
-    }
-
-    @Override
-    protected void onPostResume() {
-        super.onPostResume();
-        String status = rfidHandler.onResume();
-        statusTextViewRFID.setText(status);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        rfidHandler.onDestroy();
-    }
-
-    @Override
-    public void handleTagdata(TagData[] tagData) {
-        final StringBuilder sb = new StringBuilder();
-        for (int index = 0; index < tagData.length; index++) {
-            sb.append(tagData[index].getTagID() + "\n");
-        }
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                Toast.makeText(getApplicationContext(), sb.toString(), Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-    @Override
-    public void handleTriggerPress(boolean pressed) {
-        if (pressed) {
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    textrfid.setText("");
-                }
-            });
-            rfidHandler.performInventory();
-        } else
-            rfidHandler.stopInventory();
-    }
 }
+
