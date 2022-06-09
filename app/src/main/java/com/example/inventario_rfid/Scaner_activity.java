66363383a96b0a55e7964635ad9fd01e44b7ce59
Regy <52638916+Regy25@users.noreply.github.com>
2022-06-08 23:24:25 -0400

    Cambios Gradle y RFID import

--- app/src/main/java/com/example/inventario_rfid/Scaner_activity.java
+++ app/src/main/java/com/example/inventario_rfid/Scaner_activity.java
@@ -8,13 +8,17 @@ import android.widget.Button;
 import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.zebra.rfid.api3.TagData;
 
-public class Scaner_activity extends AppCompatActivity {
+
+public class Scaner_activity extends AppCompatActivity implements RFIDHandler.ResponseHandlerInterface {
+    public TextView statusTextViewRFID = null;
+    RFIDHandler rfidHandler;
+    private TextView textrfid;
+    final static String TAG = "RFID_SAMPLE";
 
 
     @Override
@@ -22,6 +26,12 @@ public class Scaner_activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scaner);
 
+        // UI
+        statusTextViewRFID = findViewById(R.id.txt_confirm_ant);
+        textrfid = findViewById(R.id.txt_tag);
+        // RFID Handler
+        rfidHandler = new RFIDHandler();
+        rfidHandler.onCreate(this);
 
         Button btn_volver = (Button) findViewById(R.id.btn_volver_scan);
         btn_volver.setOnClickListener(new View.OnClickListener() {
@@ -29,6 +39,55 @@ public class Scaner_activity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        rfidHandler.onPause();
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        String status = rfidHandler.onResume();
+        statusTextViewRFID.setText(status);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        rfidHandler.onDestroy();
+    }
+
+
+    @Override
+    public void handleTagdata(TagData[] tagData) {
+        final StringBuilder sb = new StringBuilder();
+        for (int index = 0; index < tagData.length; index++) {
+            sb.append(tagData[index].getTagID() + "\n");
+        }
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), sb.toString(), Toast.LENGTH_SHORT).show();
+//                textrfid.append(sb.toString());
+            }
+        });
+    }
+
+    @Override
+    public void handleTriggerPress(boolean pressed) {
+        if (pressed) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    textrfid.setText("");
+                }
+            });
+            rfidHandler.performInventory();
+        } else
+            rfidHandler.stopInventory();
     }
 }
 
