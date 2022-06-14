2d8034e5ebbba4a7f02e381bfc87cdb47079aec4
Josue Ramos <josu.ramos@duocuc.cl>
2022-06-13 22:23:53 -0400

    Update Reportes_activity.java
    Carga de datos buged

--- app/src/main/java/com/example/inventario_rfid/Reportes_activity.java
+++ app/src/main/java/com/example/inventario_rfid/Reportes_activity.java
@@ -57,12 +57,28 @@ public class Reportes_activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         db = DBHelper.getInstance(this);
 
+        Pareados ppBig = new Pareados();
+        ppBig.tag_par = "1234";
+        ppBig.id_user = 1;
+        ppBig.id_item = 1;
+        ppBig.id_pos = 1;
+        ppBig.fec_creacion = "12/06/2022";
+        ppBig.fec_modificacion = "12/06/2022";
+        ppBig.fec_salida = "";
+        ppBig.esSalida = 0;
+        db.addOrUpdatePareados(ppBig);
+        ppBig.tag_par = "2345";
+        db.addOrUpdatePareados(ppBig);
+        ppBig.tag_par = "3456";
+        db.addOrUpdatePareados(ppBig);
+
         setContentView(R.layout.activity_reportes);
         dateText = findViewById(R.id.txt_fecha_ini);
         dateText2 = findViewById(R.id.txt_fecha_fin);
-        Button btn_cargar = (Button) findViewById(R.id.btn_cargarDatos);
-        btn_cargar.setOnClickListener(new View.OnClickListener() {
-            @Override
+
+
+        Button bn = (Button) findViewById(R.id.btn_cargarDatos);
+        bn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 try {
                     MostrarDatos(dateText.toString(), dateText2.toString());
@@ -71,7 +87,6 @@ public class Reportes_activity extends AppCompatActivity {
                 }
             }
         });
-
         findViewById(R.id.txt_btn_ini).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -120,10 +135,10 @@ public class Reportes_activity extends AppCompatActivity {
             mMonth1 = month;
             mDay1 = day;
             // show selected date to date button
-            dateText.setText(new StringBuilder()
-                    .append(mYear1).append("-")
-                    .append(mMonth1 + 1).append("-")
-                    .append(mDay1).append(" "));
+//            dateText.setText(new StringBuilder()
+//                    .append(mYear1).append("-")
+//                    .append(mMonth1 + 1).append("-")
+//                    .append(mDay1).append("-"));
             String test = dateText.toString();
         }
     }
@@ -152,7 +167,7 @@ public class Reportes_activity extends AppCompatActivity {
             mDay2 = day;
             // show selected date to date button
             dateText2.setText(new StringBuilder()
-                    .append(mDay2).append(" ")
+                    .append(mDay2).append("-")
                     .append(mMonth2 + 1).append("-")
                     .append(mYear2).append("-"));
             String test = dateText2.toString();
@@ -166,34 +181,62 @@ public class Reportes_activity extends AppCompatActivity {
         Context context = getApplicationContext();
         TableLayout tbl_datos = (TableLayout) findViewById(R.id.tbl_datos);
 
-
+        DateText1 = "01/01/2022";
+        DateText2 = "01/01/2023";
+        List<Pareados> pa = db.getAllPareados();
+        List<Item> it = db.getAllItem();
 
         try {
             if (ValidacionDatos(DateText1, DateText2)) {
 
                 // AÑADIR LAS QUERYS PARA HACER EL SELECT E INSERTAR EN LA TABLA VISUALMENTE.
-                TableRow fila =new TableRow(this);
+
+                TextView tax;
                 TextView tex;
+                TextView tix;
+                TextView tox;
+                TextView tux;
+
 
 
                 //Con esta seccion se añaden los datos desde la tabla de bd hacia acá
 
                 List<PareadosFecha> lista= db.PareadosFecha(DateText1, DateText2);
 
+
                 for (PareadosFecha Par : lista){
                     //fila.addView(Par);
+                    TableRow fila =new TableRow(this);
+                    tax = new EditText(this);
                     tex = new EditText(this);
+                    tix = new EditText(this);
+                    tox = new EditText(this);
+                    tux = new EditText(this);
+                    tax.setGravity(Gravity.CENTER_VERTICAL);
                     tex.setGravity(Gravity.CENTER_VERTICAL);
-                    tex.setText(Par.id_pf);
+                    tix.setGravity(Gravity.CENTER_VERTICAL);
+                    tox.setGravity(Gravity.CENTER_VERTICAL);
+                    tux.setGravity(Gravity.CENTER_VERTICAL);
+
+                    tax.setText(String.valueOf(Par.id_pf));
+                    tex.setText(Par.tag_pf);
+                    tix.setText(Par.item_pf);
+                    tox.setText(Par.fecha_pf);
+                    tux.setText(Par.esSalida);
 
                     //tex.setText(Par[0]);
+                    fila.addView(tax);
                     fila.addView(tex);
+                    fila.addView(tix);
+                    fila.addView(tox);
+                    fila.addView(tux);
                     //tbl_datos.addView(context, Par);
+                    tbl_datos.addView(fila);
 
                 }
 
 
-                tbl_datos.addView(fila);
+
 
 
             } else {
